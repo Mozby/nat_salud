7ca57a1263697ec6998106f1202d731db13c6eb5
EdManDev <ed.manigat@gmail.com>
2019-06-09 00:31:29 -0400

    my test

--- src/components/header.js
+++ src/components/header.js
@@ -2,46 +2,63 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./img/logo.png";
 
-import "./css/header.css";
-import "./css/mininav.css";
+import { FaAlignJustify } from "react-icons/fa";
+
+// import "./css/header.css";
+// import "./css/mininav.css";
 import "./css/hover.css";
+import "./css/hamberger.css";
 
 export default class header extends Component {
+  state = {
+    isOpen: false
+  };
+  handleToogle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
     return (
-      <div>
-        <header>
-          <NavLink to="/home">
-            <div className="logo">
-              <img width="30" src={Logo} title="Home" alt="" />
-              Natural Salud
-            </div>
-          </NavLink>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to="/home" className="hover">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/Service" className="hover">
-                  Service
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/about" className="hover">
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/Contact" className="hover">
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
+      <div className="navbar">
+        <div className="nav-center">
+          <div className="navheader">
+            {/* <NavLink to="/home">
+              <div className="logo">
+                <img width="30" src={Logo} title="Home" alt="" />
+                Natural Salud
+              </div>
+            </NavLink> */}
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToogle}
+            >
+              <FaAlignJustify className="nav-icon" />
+            </button>
+          </div>
+        </div>
+        <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+          <li>
+            <NavLink to="/contact" className="hover">
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/service" className="hover">
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="hover">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/home" className="hover">
+              Home
+            </NavLink>
+          </li>
+        </ul>
       </div>
     );
   }
